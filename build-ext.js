8b7173b89be15701e09ec1d8ceff51daa9a72d24
Nicholas Jitkoff <nicholas@jitkoff.com>
2022-12-28 00:00:17 -0800

    Add bookmarklet

--- build-ext.js
+++ build-ext.js
@@ -4,14 +4,18 @@ const fs = require('fs');
 
 //process.chdir("docs");
 
-execSync('terser chrome/open_in_bitty.js > bookmarklet/open_in_bitty.js')
+execSync('terser chrome/open_in_bitty.js > bookmarklet/open_in_bitty.min.js')
 
-let data = fs.readFileSync('bookmarklet/open_in_bitty.js', 'utf8');
+let data = fs.readFileSync('bookmarklet/open_in_bitty.min.js', 'utf8');
+let url = `https://itty.bitty.app/itty.bitty-recipes/d/Send-recipes-to-itty.bitty-to-clean-them-up-and-make-them-available-offline/f/%F0%9F%8D%B3/#Make_itty.bitty/javascript:` + encodeURIComponent(data);
 let shortcut = `[InternetShortcut]
-URL=https://itty.bitty.app/itty.bitty-recipes/d/Send-recipes-to-itty.bitty-to-clean-them-up-and-make-them-available-offline/f/%F0%9F%8D%B3/#Make_itty.bitty/javascript:`
-shortcut += encodeURIComponent(data)
+URL=` + url
 
 fs.writeFileSync("bookmarklet/open_in_bitty.url",shortcut)
+fs.writeFileSync("docs/bookmarklet.html", `<meta http-equiv="Refresh" content="0; url='${url}'" />`
+
+)
+
 
 execSync('zip -r chrome.zip chrome')
 
